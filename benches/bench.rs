97f648aad66f60c36d2ca762eab2110408feef27
Alan Somers <asomers@gmail.com>
2017-05-05 22:57:50 -0600

    Add another benchmark that simulates an uncongested thread pool

--- benches/bench.rs
+++ benches/bench.rs
@@ -7,12 +7,14 @@ extern crate tokio_file;
 extern crate test;
 
 use futures::sync::oneshot;
+use std::cell::RefCell;
 use std::fs;
 use std::io::Write;
 use std::os::unix::fs::FileExt;
 use std::rc::Rc;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, mpsc};
 use std::thread;
+use std::vec;
 use tempdir::TempDir;
 use test::Bencher;
 use tokio_core::reactor::Core;
@@ -70,5 +72,58 @@ fn bench_threaded_read(bench: &mut Bencher) {
     });
 }
     
+struct TpOpspec {
+    f:  Arc<Mutex<fs::File>>,
+    buf: RefCell<vec::Vec<u8>>,
+    offset: u64,
+    tx: oneshot::Sender<usize>
+}
 
+// For comparison, benchmark the equivalent operation using a single thread to
+// simulate a thread pool
+#[bench]
+fn bench_threadpool_read(bench: &mut Bencher) {
+    // First prep the test file
+    let dir = TempDir::new("tokio-file").unwrap();
+    let path = dir.path().join("threadpool_read");
+    let mut f = fs::File::create(&path).unwrap();
+    let wbuf = vec![0; FLEN];
+    f.write(&wbuf).expect("write failed");
 
+    // Prep the thread "pool"
+    let (_ptx, prx) = mpsc::channel();
+    let ptx = Arc::new(_ptx);
+    thread::spawn(move || {
+        loop {
+            let v: Option<TpOpspec> = prx.recv().unwrap();
+            if let Some(op) = v {
+                let mut buf = op.buf.borrow_mut();
+                let f = op.f.lock().unwrap();
+                let len: usize = f.read_at(&mut buf, op.offset).unwrap();
+                op.tx.send(len).expect("send failed");
+            } else {
+                break;
+            }
+        }
+    });
+
+    // Prep the reactor
+    let mut l = Core::new().unwrap();
+    let pathclone = path.clone();
+    let file = Arc::new(Mutex::new(fs::File::open(&pathclone).unwrap()));
+    let ptxclone = ptx.clone();
+
+    bench.iter(move || {
+        let rbuf = vec![0; FLEN];
+        let (tx, rx) = oneshot::channel::<usize>();
+        let opspec = TpOpspec{
+            f: file.clone(),
+            buf: RefCell::new(rbuf),
+            offset: 0,
+            tx: tx};
+        ptxclone.send(Some(opspec)).unwrap();
+
+        assert_eq!(l.run(rx).expect("receiving failed") as usize, FLEN);
+    });
+    ptx.send(None).unwrap();
+}
