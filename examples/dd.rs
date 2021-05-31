c1e7f341f5146451ecd37ea44ce4cba7ddd2289b
Alan Somers <asomers@gmail.com>
2021-05-31 11:31:24 -0600

    Fix a panic-format warning in examples/dd

--- examples/dd.rs
+++ examples/dd.rs
@@ -54,7 +54,7 @@ fn main() {
     opts.optflag("h", "help", "print this help menu");
     let matches = match opts.parse(env::args().skip(1)) {
         Ok(m) => {m},
-        Err(f) => { panic!(f.to_string()) }
+        Err(f) => { panic!("{}", f.to_string()) }
     };
     if matches.opt_present("h") {
         usage(opts);
